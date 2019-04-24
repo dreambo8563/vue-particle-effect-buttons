926e056637b52979aac4ce62ddca6e2010f9d5a3
dreambo8563 <dreambo8563@outlook.com>
2019-04-24 23:13:20 +0800

    fix(rename): to vue-particle-effect-buttons to resolve conflict

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production"
-      ? "/vue-particle-effect-button/"
+      ? "/vue-particle-effect-buttons/"
       : "/",
   css: {
     extract: false
