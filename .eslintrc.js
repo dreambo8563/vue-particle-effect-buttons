638e6b5936afdae7492fab42684c5e09291f2f2d
dreambo8563 <dreambo8563@outlook.com>
2019-08-22 10:36:51 +0800

    feat(upgrade packages):

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,13 @@ module.exports = {
   },
   parserOptions: {
     parser: "babel-eslint"
-  }
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)"],
+      env: {
+        jest: true
+      }
+    }
+  ]
 };
--- jest.config.js
+++ jest.config.js
@@ -25,10 +25,5 @@ module.exports = {
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname"
-  ],
-  globals: {
-    "ts-jest": {
-      diagnostics: false
-    }
-  }
+  ]
 };
